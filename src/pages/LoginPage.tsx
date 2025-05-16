0575a2752187e6801ac547da730493c9799e73bc
Hoplites04 <chienbinhhylap04@gmail.com>
2025-05-16 10:38:29 +0700

    test commit

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+
+import { LoginForm } from "@/components/login-form";
+function LoginPage() {
+    return (  
+        <>
+            <LoginForm/>
+        </>
+    );
+}
+
+export default LoginPage;
--- src/routes/AppRoute.tsx
+++ src/routes/AppRoute.tsx
@@ -1,9 +1,15 @@
 import Layout from "@/components/layout";
+import LoginPage from "@/pages/LoginPage";
 import { createBrowserRouter, Navigate } from "react-router";
 
 export const router = createBrowserRouter ([
     {
         path: "/",
         element: <Layout />
+    },
+    {
+        path: "/login",
+        element: <LoginPage/>
     }
+    
 ])
