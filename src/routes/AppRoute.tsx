aa3d38c90707a4bca24a6aa6ad02050b0bbb6904
joeyngdev <trtinnguyen2009@gmail.com>
2025-05-19 14:17:07 +0700

    request page ui

--- src/routes/AppRoute.tsx
+++ src/routes/AppRoute.tsx
@@ -1,6 +1,8 @@
 import Layout from "@/components/layout";
-import UserForm from "@/features/users/components/create-user";
+import RequestPage from "@/pages/requests/RequestPage";
+import UserForm from "@/pages/users/CreateUserPage";
 import LoginPage from "@/pages/LoginPage";
+import UserMangement from "@/pages/users/UserMangementPage";
 import { createBrowserRouter, Navigate } from "react-router";
 
 export const router = createBrowserRouter([
@@ -12,6 +14,14 @@ export const router = createBrowserRouter([
         path: "/create-user",
         element: <UserForm />,
       },
+      {
+        path: "users",
+        element: <UserMangement />,
+      },
+      {
+        path: "returns",
+        element: <RequestPage/>
+      }
     ],
   },
   {
