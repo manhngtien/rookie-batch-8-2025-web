5495d4bc61c784591a51dfe4a4f22a762dcba40c
Hoplites04 <chienbinhhylap04@gmail.com>
2025-05-19 18:26:43 +0700

    change password UI

--- src/routes/AppRoute.tsx
+++ src/routes/AppRoute.tsx
@@ -1,6 +1,7 @@
 import Layout from "@/components/layout";
 import UserForm from "@/features/users/components/create-user";
-import LoginPage from "@/pages/LoginPage";
+import LoginPage from "@/pages/auth/LoginPage";
+import UserDataTable from "@/features/users/components/UserDataTable";
 import { createBrowserRouter, Navigate } from "react-router";
 
 export const router = createBrowserRouter([
@@ -12,10 +13,15 @@ export const router = createBrowserRouter([
         path: "/create-user",
         element: <UserForm />,
       },
+      {
+        path:"/users",
+        element: <UserDataTable/>,
+      }
     ],
   },
   {
     path: "/login",
     element: <LoginPage />,
   },
+
 ]);
