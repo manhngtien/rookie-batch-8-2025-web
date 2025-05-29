59a4405de0ad35e270e718665f71488bf410ea4d
Thu Nguyen <thuthien2001@gmail.com>
2025-05-29 23:21:08 +0700

    feat: assets popup on create/edit assignments

--- src/features/assignments/components/asset-select-columns.tsx
+++ src/features/assignments/components/asset-select-columns.tsx
@@ -1,9 +1,9 @@
 import type { ColumnDef } from "@tanstack/react-table";
 
 import { DataTableColumnHeader } from "@/components/ui/data-table-col-header";
-import type { User } from "@/features/users/types/User";
+import type { Asset } from "@/features/asset-management/types/Asset";
 
-export const userSelectColumns: ColumnDef<User>[] = [
+export const assetSelectColumns: ColumnDef<Asset>[] = [
   {
     id: "select",
     cell: ({ row }) => (
@@ -11,28 +11,28 @@ export const userSelectColumns: ColumnDef<User>[] = [
         id=""
         type="radio"
         checked={row.getIsSelected()}
-        value={row.original.staffCode}
+        value={row.original.assetCode}
         disabled={!row.getCanSelect()}
         onChange={row.getToggleSelectedHandler()}
       />
     ),
   },
   {
-    accessorKey: "staffCode",
+    accessorKey: "assetCode",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Staff Code" />
+      <DataTableColumnHeader column={column} title="Asset Code" />
     ),
   },
   {
-    accessorKey: "fullName",
+    accessorKey: "assetName",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Full Name" />
+      <DataTableColumnHeader column={column} title="Asset Name" />
     ),
   },
   {
-    accessorKey: "type",
+    accessorKey: "category.categoryName",
     header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="Type" />
+      <DataTableColumnHeader column={column} title="Category" />
     ),
   },
 ];
