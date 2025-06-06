0cbc79b15ff1df332bae5ca75289067a5f42948a
Thu Nguyen <thuthien2001@gmail.com>
2025-06-06 16:23:09 +0700

    fix: revert the asset detail dialog to the adjusted component before

--- src/features/asset-management/components/asset-detail-dialog.tsx
+++ src/features/asset-management/components/asset-detail-dialog.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
+import { DetailDialog } from "@/components/ui/dashboard-elements";
 import {
   type Asset,
   type ELocation,
@@ -23,92 +18,82 @@ const AssetDetailDialog: React.FC<AssetDetailDialogProps> = ({
   closeModal,
 }) => {
   return (
-    <Dialog open={!!selectedAsset} onOpenChange={closeModal}>
-      <DialogContent className="max-w-3xl p-0 text-black">
-        <DialogHeader className="w-full rounded-t-lg border-b-1 border-b-black bg-gray-200 p-4">
-          <DialogTitle className="border-red-500 pb-2 text-red-500">
-            Detailed Asset Information
-          </DialogTitle>
-        </DialogHeader>
-
-        {selectedAsset && (
-          <div className="w-full space-y-4 px-8 py-2 break-words">
-            <div className="grid grid-cols-2 gap-4 text-gray-500">
-              <p className="font-medium">Asset Code:</p>
-              <p className="text-left">{selectedAsset.assetCode}</p>
-              <p className="font-medium">Asset Name:</p>
-              <p className="text-left break-words">{selectedAsset.assetName}</p>
-              <p className="font-medium">Category:</p>
-              <p className="text-left">{selectedAsset.category.categoryName}</p>
-              <p className="font-medium">Installed Date:</p>
-              <p className="text-left">
-                {formatDate(selectedAsset.installedDate)}
-              </p>
-              <p className="font-medium">State:</p>
-              <p className="text-left">{selectedAsset.state}</p>
-              <p className="font-medium">Location:</p>
-              <p className="text-left">
-                {LocationLabelMap[selectedAsset.location as ELocation] ??
-                  "Unknown"}
-              </p>{" "}
-              <p className="font-medium">Specification:</p>
-              <p className="text-left break-words">
-                {selectedAsset.specification}
-              </p>
-            </div>
-            <div>
-              <div className="mb-2 flex flex-col items-start gap-5">
-                <h1 className="text-gray-500">History:</h1>
-                <table className="w-full text-sm">
-                  <thead>
-                    <tr className="border-b">
-                      <th className="p-2 text-left text-gray-500">Date</th>
-                      <th className="p-2 text-left text-gray-500">
-                        Assigned to
-                      </th>
-                      <th className="p-2 text-left text-gray-500">
-                        Assigned by
-                      </th>
-                      <th className="p-2 text-left text-gray-500">
-                        Return Date
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {selectedAsset.assignments.length > 0 ? (
-                      selectedAsset.assignments.map((assignment, index) => (
-                        <tr key={index} className="border-b">
-                          <td className="p-2 text-gray-500">
-                            {assignment.assignedDate}
-                          </td>
-                          <td className="max-w-3xs p-2 break-words text-gray-500">
-                            {assignment.assignedToUser.userName}
-                          </td>
-                          <td className="max-w-3xs p-2 break-words text-gray-500">
-                            {assignment.assignedByUser.userName}
-                          </td>
-                          <td className="p-2 text-gray-500">
-                            {assignment.assignedDate
-                              ? assignment.assignedDate
-                              : "N/A"}
-                          </td>
-                        </tr>
-                      ))
-                    ) : (
-                      <tr>
-                        <td colSpan={4} className="p-2 text-center">
-                          No assignment history
+    <DetailDialog<Asset>
+      selectedEntity={selectedAsset}
+      closeModal={closeModal}
+      title="Detailed Asset Information"
+    >
+      {selectedAsset && (
+        <div className="w-full space-y-4 px-8 py-2 break-words">
+          <div className="grid grid-cols-2 gap-4 text-gray-500">
+            <p className="font-medium">Asset Code:</p>
+            <p className="text-left">{selectedAsset.assetCode}</p>
+            <p className="font-medium">Asset Name:</p>
+            <p className="text-left break-words">{selectedAsset.assetName}</p>
+            <p className="font-medium">Category:</p>
+            <p className="text-left">{selectedAsset.category.categoryName}</p>
+            <p className="font-medium">Installed Date:</p>
+            <p className="text-left">
+              {formatDate(selectedAsset.installedDate)}
+            </p>
+            <p className="font-medium">State:</p>
+            <p className="text-left">{selectedAsset.state}</p>
+            <p className="font-medium">Location:</p>
+            <p className="text-left">
+              {LocationLabelMap[selectedAsset.location as ELocation] ??
+                "Unknown"}
+            </p>{" "}
+            <p className="font-medium">Specification:</p>
+            <p className="text-left break-words">
+              {selectedAsset.specification}
+            </p>
+          </div>
+          <div>
+            <div className="mb-2 flex flex-col items-start gap-5">
+              <h1 className="text-gray-500">History:</h1>
+              <table className="w-full text-sm">
+                <thead>
+                  <tr className="border-b">
+                    <th className="p-2 text-left text-gray-500">Date</th>
+                    <th className="p-2 text-left text-gray-500">Assigned to</th>
+                    <th className="p-2 text-left text-gray-500">Assigned by</th>
+                    <th className="p-2 text-left text-gray-500">Return Date</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {selectedAsset.assignments.length > 0 ? (
+                    selectedAsset.assignments.map((assignment, index) => (
+                      <tr key={index} className="border-b">
+                        <td className="p-2 text-gray-500">
+                          {assignment.assignedDate}
+                        </td>
+                        <td className="max-w-3xs p-2 break-words text-gray-500">
+                          {assignment.assignedToUser.userName}
+                        </td>
+                        <td className="max-w-3xs p-2 break-words text-gray-500">
+                          {assignment.assignedByUser.userName}
+                        </td>
+                        <td className="p-2 text-gray-500">
+                          {assignment.assignedDate
+                            ? assignment.assignedDate
+                            : "N/A"}
                         </td>
                       </tr>
-                    )}
-                  </tbody>
-                </table>
-              </div>
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan={4} className="p-2 text-center">
+                        No assignment history
+                      </td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
             </div>
           </div>
-        )}
-      </DialogContent>
-    </Dialog>
+        </div>
+      )}
+    </DetailDialog>
   );
 };
 
